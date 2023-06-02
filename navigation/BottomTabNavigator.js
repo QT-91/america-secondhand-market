b6c54e2f3c2c22727d077b791978bdc5379c6c88
Minseo Kim <68639278+star14ms@users.noreply.github.com>
2023-06-02 13:59:12 -0400

    Feat: Use Library <react-native-maps>

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -8,6 +8,7 @@ import { useColorScheme } from "react-native";
 import Colors from "../constants/Colors";
 import TabOneScreen from "../screens/TabOneScreen";
 import TabTwoScreen from "../screens/TabTwoScreen";
+import MapScreen from "../screens/MapScreen";
 
 const BottomTab = createBottomTabNavigator();
 
@@ -16,12 +17,22 @@ export default function BottomTabNavigator() {
 
   return (
     <BottomTab.Navigator
-      initialRouteName="TabOne"
+      initialRouteName="Maps"
       screenOptions={{ tabBarActiveTintColor: Colors[colorScheme].tint }}
     >
       <BottomTab.Screen
         name="TabOne"
         component={TabOneNavigator}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="ios-code" color={color} />
+          ),
+        }}
+      />
+      <BottomTab.Screen
+        name="Maps"
+        component={MapScreen}
         options={{
           headerShown: false,
           tabBarIcon: ({ color }) => (
